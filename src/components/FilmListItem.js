73b695259783796149e62f2e76a9a6f8bc373cc3
wisara88 <srandall845@gmail.com>
2020-02-08 14:34:28 -0800

    axios GET request working

--- src/components/FilmListItem.js
+++ src/components/FilmListItem.js
@@ -1,9 +1,17 @@
 import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 export default function FilmListItem(props) {
 
   const [film, setFilm] = useState([]);
  
+  useEffect( () => {
+      axios.get("https://swapi.co/api/films/1/").then((response) => {
+        console.log(response.data.title);
+      });
+    }
+  )
+
   return (
     <p>hello</p>
   );
