82eff3cf5eeadca487d6d8e10c53edab3c0a5336
wisara88 <srandall845@gmail.com>
2020-02-11 20:28:30 -0800

    charDetails update

--- src/components/CharDetails.js
+++ src/components/CharDetails.js
@@ -6,11 +6,14 @@ export default function CharDetails(props) {
 
   return (
     <div>
-      <p>char</p>
-      <p>{props.charID.name}</p>
-      <p>{props.charID.height}</p>
-      <p>{props.charID.mass}</p>
-
+      <h2>Character Detail Page</h2>
+      <p>Name: {props.charID.name}</p>
+      <p>Height: {props.charID.height}</p>
+      <p>Mass: {props.charID.mass}</p>
+      <p>Hair Colour: {props.charID.hair_colour}</p>
+      <p>Eye Colour: {props.charID.eye_colour}</p>
+      <p>Birth Year: {props.charID.birth_year}</p>
+      <p>Gender: {props.charID.gender}</p>
     </div>
   );
 }
