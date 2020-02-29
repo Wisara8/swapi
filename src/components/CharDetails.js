0da2554a000b299b872cf5a865423e496180f080
wisara88 <srandall845@gmail.com>
2020-02-28 19:23:53 -0800

    CharDetails code refractor

--- src/components/CharDetails.js
+++ src/components/CharDetails.js
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function CharDetails(props) {
 
-  console.log("charID: ", props.charID);
+  const { charID } = props;
 
   return (
     <div>
       <h2>Character Detail Page</h2>
-      <p>Name: {props.charID.name}</p>
-      <p>Height: {props.charID.height}</p>
-      <p>Mass: {props.charID.mass}</p>
-      <p>Hair Colour: {props.charID.hair_colour}</p>
-      <p>Eye Colour: {props.charID.eye_colour}</p>
-      <p>Birth Year: {props.charID.birth_year}</p>
-      <p>Gender: {props.charID.gender}</p>
+      <p>Name: {charID.name}</p>
+      <p>Height: {charID.height}</p>
+      <p>Mass: {charID.mass}</p>
+      <p>Hair Colour: {charID.hair_colour}</p>
+      <p>Eye Colour: {charID.eye_colour}</p>
+      <p>Birth Year: {charID.birth_year}</p>
+      <p>Gender: {charID.gender}</p>
     </div>
   );
 }
